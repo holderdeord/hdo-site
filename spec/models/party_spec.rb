813131d7748319f74a5aaef487afeef47b51ad99
Jari Bakken <jari.bakken@gmail.com>
2012-04-07 12:44:24 +0200

    Replace pending spec (testing travis)

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Party do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "returns its percentage of the parliament" do
+    Representative.stub!(:count => 100)
+
+    party = Party.create(:name => "Democratic Party")
+    Representative.create(:first_name => "Donald", :last_name => "Duck", :party => party)
+
+    party.percent_of_representatives.should == 1
+  end
 end
