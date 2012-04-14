976d06f90da16db15f1a443ee00ebb2701403506
Jari Bakken <jari.bakken@gmail.com>
2012-04-14 17:17:49 +0200

    Sort a representative's votes by date.

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -14,6 +14,9 @@ class RepresentativesController < ApplicationController
   def show
     @representative = Representative.includes(:votes => :issue).find(params[:id])
 
+    # TODO: do this in DB?
+    @vote_results = @representative.vote_results.sort_by { |e| e.vote.time }.reverse
+
     respond_to do |format|
       format.html
       format.json { render json: @representative }
