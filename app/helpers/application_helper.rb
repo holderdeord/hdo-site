eca32ca9ffa2f846ce17cae7ba6e8a27cfff5089
Jari Bakken <jari.bakken@gmail.com>
2012-04-20 12:57:54 +0100

    Obscure emails, add links on about page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def obscure_email(email)
+    return nil if email.nil?
+
+    lower = ('a'..'z').to_a
+    upper = ('A'..'Z').to_a
+
+    email.split('').map { |char|
+      output = lower.index(char) + 97 if lower.include?(char)
+      output = upper.index(char) + 65 if upper.include?(char)
+      output ? "&##{output};" : (char == '@' ? '&#0064;' : char)
+    }.join.html_safe
+  end
 end
