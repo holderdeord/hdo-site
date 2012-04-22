95dfc6490681dd30e2264d974d594df15b3bd6de
Jari Bakken <jari.bakken@gmail.com>
2012-04-22 17:12:55 +0200

    Show a notice on unanimous votes.

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -12,7 +12,7 @@ class PartiesController < ApplicationController
   end
 
   def show
-    @party = Party.find(params[:id])
+    @party = Party.includes(:representatives, :promises => :topics).find(params[:id])
 
     respond_to do |format|
       format.html
