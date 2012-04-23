1171dfb9fea5743743a642ecfc4020bffb6171e5
Jari Bakken <jari.bakken@gmail.com>
2012-04-23 13:15:27 +0200

    Remove locale filter.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  # FIXME: just for demo purposes - this switches the local globally (i.e across sessions)
-  before_filter :set_locale
-
-  def set_locale
-    unless [nil, I18n.locale].include? params[:locale]
-      I18n.locale = params[:locale]
-    end
-  end
 end
