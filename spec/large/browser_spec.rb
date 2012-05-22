holderdeord/hdo-site

commit 573866c28961731e2c45ede4cc87e2cd9068a061

Handle more than one issue per vote. Closes #9.

app/controllers/votes_controller.rb
@@ -2,7 +2,7 @@ class VotesController < ApplicationController
   caches_page :index, :show
 
   def index
-    @votes = Vote.includes(:issue).order(:time).reverse_order
+    @votes = Vote.includes(:issues).order(:time).reverse_order
 
     respond_to do |format|
       format.html

app/views/issues/show.html.erb
@@ -61,4 +61,4 @@
 </ul>
 
 <h2><%= Vote.model_name.human count: 2 %></h2>
-<%= render partial: "shared/votes_summary_table", locals: {votes: @issue.votes, show_issue: false }%>
+<%= render partial: "shared/votes_summary_table", locals: {votes: @issue.votes, show_issues: false }%>

app/models/issue.rb
@@ -1,7 +1,7 @@
 class Issue < ActiveRecord::Base
   belongs_to :committee
   has_and_belongs_to_many :topics
-  has_many :votes
+  has_and_belongs_to_many :votes
 
   def status_text
     status.gsub(/_/, ' ').capitalize

app/models/vote.rb
@@ -1,11 +1,12 @@
 class Vote < ActiveRecord::Base
-  belongs_to :issue
-  validates_presence_of :issue
+  has_and_belongs_to_many :issues
+  validates_length_of :issues, :minimum => 1
 
   has_many :propositions, :dependent => :delete_all
   has_many :vote_results, :dependent => :delete_all
   has_many :representatives, :through => :vote_results, :order => :last_name
 
+  # should be not_personal
   scope :not_unanimous, where('for_count != 0 AND against_count != 0 AND absent_count != 0')
 
   def time_text
@@ -16,6 +17,7 @@ class Vote < ActiveRecord::Base
     enacted? ? I18n.t('app.yes') : I18n.t('app.no')
   end
 
+  # TODO: should be "#personal?", though rare, votes can be unanimous and still have personal votes
   def unanimous?
     for_count == 0 && against_count == 0 && absent_count == 0
   end

app/views/shared/_votes_summary_table.html.erb
@@ -5,7 +5,7 @@
     <thead>
       <tr>
         <th class="wide-table-header"><%= Vote.human_attribute_name :time %></th>
-        <% if show_issue %>
+        <% if show_issues %>
           <th><%= Issue.model_name.human %></th>
         <% end %>
         <th><%= Vote.human_attribute_name :subject %></th>
@@ -19,8 +19,15 @@
       <% votes.each do |vote| %>
       <tr>
         <td><%= vote.time_text %></td>
-        <% if show_issue %>
-          <td><%= link_to vote.issue.summary, vote.issue %></td>
+        <% if show_issues %>
+          <td>
+            <ul>
+              <%- vote.issues.each do |issue| -%>
+              <li><%= link_to issue.summary, issue %></li>
+              <%- end -%>
+            </ul>
+
+          </td>
         <% end %>
         <td><%= link_to vote.subject, vote %></td>
         <td><%= vote.enacted_text %></td>

app/views/votes/index.html.erb
@@ -2,4 +2,4 @@
   <h1><%= Vote.model_name.human count: 2 %></h1>
 </div>
 
-<%= render partial: "shared/votes_summary_table", locals: {votes: @votes, show_issue: true } %>
+<%= render partial: "shared/votes_summary_table", locals: {votes: @votes, show_issues: true } %>

db/migrate/20120407220757_district_representative_association.rb
@@ -4,6 +4,6 @@ class DistrictRepresentativeAssociation < ActiveRecord::Migration
   end
 
   def down
-    remove_column :representatives, :district_id, :integer
+    remove_column :representatives, :district_id
   end
 end

db/migrate/20120515184039_many_to_many_issues_and_votes.rb
@@ -1,0 +1,16 @@
+class ManyToManyIssuesAndVotes < ActiveRecord::Migration
+  def up
+    remove_column :votes, :issue_id
+
+    create_table :issues_votes, :id => false do |t|
+      t.references :issue, :vote
+    end
+
+    add_index :issues_votes, [:vote_id, :issue_id]
+  end
+
+  def down
+    drop_table :issues_votes
+    add_column :votes, :issue_id
+  end
+end

commit be78d92808d406ab7e5353e9b701c2d8d9e093c2

Fix a few bugs introduced in 573866c.

app/controllers/representatives_controller.rb
@@ -12,7 +12,7 @@ class RepresentativesController < ApplicationController
   end
 
   def show
-    @representative = Representative.includes(:votes => :issue).find(params[:id])
+    @representative = Representative.includes(:votes => :issues).find(params[:id])
     @vote_results = @representative.vote_results.sort_by { |result| result.vote.time }
 
     respond_to do |format|

app/controllers/votes_controller.rb
@@ -13,11 +13,11 @@ class VotesController < ApplicationController
 
   def show
     @vote = Vote.includes(
-      :issue, :vote_results => {:representative => :party},
+      :issues, :vote_results => {:representative => :party},
     ).find(params[:id])
 
-    @issue = @vote.issue
-    @stats = @vote.stats
+    @issues = @vote.issues
+    @stats  = @vote.stats
 
     respond_to do |format|
       format.html

app/views/representatives/show.html.erb
@@ -62,8 +62,15 @@
     <% @vote_results.reverse_each do |result| %>
     <tr>
       <td><%= result.vote.time_text %></td>
-      <td><%= link_to result.vote.issue.summary, result.vote.issue %></td>
       <td><%= link_to result.vote.subject, result.vote %></td>
+      <td>
+        <ul>
+          <%- result.vote.issues.each do |issue| -%>
+          <li><%= link_to issue.summary, issue %></li>
+          <%- end -%>
+        </ul>
+
+      </td>
       <td><%= result.vote.enacted_text %></td>
       <td class="vote-<%= result.state %>"><%= result.human %></td>
     </tr>

app/views/votes/show.html.erb
@@ -17,7 +17,9 @@
         <dd><%= @vote.time %></dd>
 
         <dt><%= Issue.model_name.human %></dt>
-        <dd><%= link_to @vote.issue.summary, @vote.issue %></dd>
+        <% @issues.each do |issue| %>
+        <dd><%= link_to issue.summary, issue %></dd>
+        <% end %>
 
         <dt><%= Vote.human_attribute_name :enacted %></dt>
         <dd><%= @vote.enacted_text %></dd>
@@ -35,8 +37,10 @@
 
 <h2>stortinget.no</h2>
 <ul class="nav nav-tabs">
-  <li><a href="<%= @vote.issue.url %>">Sak</a></li>
   <li><a href="<%= @vote.minutes_url %>">Referat</a></li>
+  <% @issues.each_with_index do |issue, idx| %>
+    <li><a href="<%= issue.url %>">Sak <%= idx + 1 %></a></li>
+  <% end %>
 </ul>
 
 <table class="table table-striped">

config/deploy.rb
@@ -59,6 +59,7 @@ namespace :clear do
 end
 
 namespace :cache do
+  desc 'Clear the page cache.'
   task(:clear) { run "rm -r #{current_path}/public/cache/*"}
 end
 

spec/large/browser_spec.rb
@@ -6,7 +6,15 @@ describe "Front page" do
     front_page.get
   end
 
-  it "should load the votes page" do
+  it "should show the list of representatives" do
+    pending
+  end
+
+  it "should show a specific representative" do
+    pending
+  end
+
+  it "should show a list of votes" do
     # TODO: use machinist, factory_girl?
     Vote.create! :for_count     => 50,
                  :against_count => 50,
@@ -23,5 +31,9 @@ describe "Front page" do
     vote.for.should == "50% (50/100)"
     vote.against.should == "50% (50/100)"
     vote.absent.should == "40% (69/169)"
+  end
+
+  it "should show a specific vote" do
+    pending
   end
 end