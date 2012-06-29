680ba0c0cfd3e831cc07f1818ad32513fa98dabb
Jari Bakken <jari.bakken@gmail.com>
2012-06-30 00:53:54 +0200

    Fix typo in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(:email => "test@holerdeord.no", :password => "hdo123", :password_confirmation => "hdo123", :remember_me => false)
+User.create!(:email => "test@holderdeord.no", :password => "hdo123", :password_confirmation => "hdo123", :remember_me => false)
