88ca044c780ae0d75d7a442aa3300bc15c18be58
Jari Bakken <jari.bakken@gmail.com>
2012-06-29 21:54:14 +0200

    Remove :registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
-         :registerable,
          :recoverable, 
          :rememberable, 
          :trackable, 
