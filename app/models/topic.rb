a14d7d5642db03d99727b53cd8156dff1132ee0a
Jari Bakken <jari.bakken@gmail.com>
2012-06-30 19:07:45 +0200

    Topic page tweaks.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ActiveRecord::Base
-  attr_accessible :description, :title
+  attr_accessible :description, :title, :category_ids
   validates_presence_of :title
 
   has_and_belongs_to_many :fields
