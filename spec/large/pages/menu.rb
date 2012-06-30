fb2e7acafcaa0098a6fb693e4cf4e9f64d0565e6
Jari Bakken <jari.bakken@gmail.com>
2012-06-30 14:03:41 +0200

    Fix browser spec.

--- spec/large/pages/menu.rb
+++ spec/large/pages/menu.rb
@@ -3,18 +3,23 @@ module Pages
     def initialize(driver)
       @driver = driver
     end
-    
+
     def open_votes
+      open_stortinget
       navbar_element.find_element(css: 'a[href="/votes"]').click
     end
-    
+
     private
-    
+
+    def open_stortinget
+      navbar_element.find_element(:css, 'a[href="#stortinget"]').click
+    end
+
     def navbar_element
       @driver.find_element :class, 'navbar'
     end
   end
-  
+
   module HasMenu
     def menu
       Menu.new driver
