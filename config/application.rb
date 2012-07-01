dfb25b1dd15dda03f7ebd4a6f19ab42eff947e38
fijoholj <jostein.holje@finn.no>
2012-07-01 13:16:12 +0200

    Added NeoSansStd-Regular font to navigation header.

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,7 @@ module Hdo
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
