4c34d693eccb73721f09ee454f9d4f21dd024df2
Jari Bakken <jari.bakken@gmail.com>
2012-07-02 02:05:20 +0200

    Made Topic model more idiomatic + convert tabs to spaces.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -9,45 +9,45 @@ class Topic < ActiveRecord::Base
   has_many :vote_directions
   has_many :votes, :through => :vote_directions, :order => :time
 
+  attr_writer :current_step
+
   def steps
     %w[categories promises votes]
   end
 
   def current_step
-  	@current_step || steps.first
+    @current_step || steps.first
   end
 
-  attr_writer :current_step
-
   def next_step
-  	self.current_step = steps[steps.index(current_step)+1]
+    self.current_step = steps[steps.index(current_step) + 1]
   end
 
   def previous_step
-  	self.current_step = steps[steps.index(current_step)-1]
+    self.current_step = steps[steps.index(current_step) - 1]
   end
 
   def first_step?
-  	current_step == steps.first
+    current_step == steps.first
   end
 
   def last_step?
-  	current_step == steps.last
+    current_step == steps.last
   end
 
   def stats
     @stats ||= Hdo::Stats::TopicCounts.new(self)
   end
 
-  def is_vote_for?(vote_id)
-    vote_directions.collect {|vd| vd.vote.id if vd.matches}.include?(vote_id)
-  end
-  
-  def is_vote_against?(vote_id)
-    vote_directions.collect {|vd| vd.vote.id unless vd.matches}.include?(vote_id)
+  def vote_for?(vote_id)
+    vote_directions.any? { |vd| vd.matches? && vd.vote_id == vote_id  }
   end
 
-  def is_current_step?(step)
+  def vote_against?(vote_id)
+    vote_directions.any? { |vd| !vd.matches? && vd.vote_id == vote_id }
+  end
+
+  def current_step?(step)
     step == self.current_step
   end
 end
