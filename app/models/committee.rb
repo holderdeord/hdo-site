8ad2a32f69624e0e1f5792a1a8c9f7afcd2d3b93
Jari Bakken <jari.bakken@gmail.com>
2012-07-02 03:36:31 +0200

    Show issues on committee#show.

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -1,6 +1,6 @@
 class Committee < ActiveRecord::Base
   has_and_belongs_to_many :representatives
-  has_many :issues
+  has_many :issues, :order => :last_update
 
   validates_uniqueness_of :name
 end
