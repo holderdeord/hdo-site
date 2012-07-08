4888d2b4d74c32b389e28043dc0e207df3c31ff7
Erik E. Seierstad <erik.seierstad@gmail.com>
2012-07-08 07:04:07 +0200

    sort representative list by party or district

--- app/assets/javascripts/representatives.js
+++ app/assets/javascripts/representatives.js
@@ -57,3 +57,86 @@ PresenceStatsGraph.prototype.render = function() {
     series: this.options.series
   });
 };
+
+var HDO = HDO || {};
+HDO.representatives = HDO.representatives || {};
+
+$(document).ready(function(){
+	var district_regex = /.*(district-[^ \t]+).*/,
+	    party_regex = /.*(party-[^ \t]+).*/,
+            sort_by_district_link = $('<a />'),
+            sort_by_party_link = $('<a />');
+
+	HDO.representatives.districts = {};
+	HDO.representatives.parties = {};
+        sort_by_district_link.text(HDO.representatives.sortByDistrictText);
+        sort_by_party_link.text(HDO.representatives.sortByPartyText);
+
+	$('.page-header').append(sort_by_district_link);
+	$('.page-header').append(sort_by_party_link);
+	
+	$('.representatives-list .representative').each(function(index, element) {
+		var $e, classes, district, party, district_class, party_class, regex_result;
+		$e = $(element);
+		classes = $e.attr('class');
+		regex_result = district_regex.exec(classes);
+		district_class = regex_result[1];
+		district = HDO.representatives.districts[district_class] = HDO.representatives.districts[district_class] || {};
+		district.name = district.name || $e.find('.district').first().text();
+		district.representatives = district.representatives || [];
+		district.representatives.push($e);
+
+		regex_result = party_regex.exec(classes);
+		party_class = regex_result[1];
+		party = HDO.representatives.parties[party_class] = HDO.representatives.parties[party_class] || {};
+		party.name = party.name || $e.find('.org').first().text();
+		party.representatives = party.representatives || [];
+		party.representatives.push($e);
+		
+	});
+
+	HDO.representatives.sortByDistrict = function() {
+		var districts = HDO.representatives.districts, 
+                    district_key,
+                    sorted_dom_elements = $('<div class="wrapper" />'),
+                    list,  
+                    i, j;
+
+		for (district_key in districts) {
+			if(districts.hasOwnProperty(district_key)) {
+				sorted_dom_elements.append($('<h2 class="representatives-sort-criteria-heading">' + districts[district_key].name + '</h2>'));
+				list = $('<ul class="representatives-list" />');
+                                sorted_dom_elements.append(list);
+				for (i = 0, j = districts[district_key].representatives.length; i < j; i += 1) {
+					list.append(districts[district_key].representatives[i].clone());
+                                }
+			}
+		}
+		$('.representatives-list').remove();
+                $('.page-header').after(sorted_dom_elements);
+	};
+	HDO.representatives.sortByParty = function() {
+		var parties = HDO.representatives.parties, 
+                    party_key,
+                    sorted_dom_elements = $('<div class="wrapper" />'),
+                    list,  
+                    i, j;
+
+		for (party_key in parties) {
+			if(parties.hasOwnProperty(party_key)) {
+				sorted_dom_elements.append($('<h2 class="representatives-sort-criteria-heading">' + parties[party_key].name + '</h2>'));
+				list = $('<ul class="representatives-list" />');
+                                sorted_dom_elements.append(list);
+				for (i = 0, j = parties[party_key].representatives.length; i < j; i += 1) {
+					list.append(parties[party_key].representatives[i].clone());
+                                }
+			}
+		}
+		$('.representatives-list').remove();
+                $('.representatives-sort-criteria-heading').remove();
+                $('.page-header').after(sorted_dom_elements);
+	};
+        sort_by_district_link.bind('click', HDO.representatives.sortByDistrict);
+        sort_by_party_link.bind('click', HDO.representatives.sortByParty);
+});
+
