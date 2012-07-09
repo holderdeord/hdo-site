9192a5ce53cf4e157ac5f8cb3ce3eb0223819ba8
Jari Bakken <jari.bakken@gmail.com>
2012-07-09 11:38:02 +0200

    Re-add the presence graph/percentages, but with a new disclaimer.

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -12,10 +12,13 @@ class RepresentativesController < ApplicationController
   end
 
   def show
-    @representative = Representative.includes(:votes => :issues).find(params[:id])
-    @vote_results = @representative.vote_results.sort_by { |result| result.vote.time }.reverse.
-                                                 paginate(:page => params[:page])
-
+    @representative  = Representative.includes(:votes => :issues).find(params[:id])
+    
+    all_vote_results = @representative.vote_results.sort_by { |result| result.vote.time }.reverse
+    @activity_stats  = Hdo::Charts::Activity.new(@representative.full_name, all_vote_results)
+    
+    @vote_results = all_vote_results.paginate(:page => params[:page])
+    
     respond_to do |format|
       format.html
       format.json { render json: @representative }
