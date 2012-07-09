ef9eaf7a06c0ffdfe9159cc980982e25bc7ce8df
Jari Bakken <jari.bakken@gmail.com>
2012-07-09 12:07:11 +0200

    Remove graph, leave percentages.

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -15,8 +15,6 @@ class RepresentativesController < ApplicationController
     @representative  = Representative.includes(:votes => :issues).find(params[:id])
     
     all_vote_results = @representative.vote_results.sort_by { |result| result.vote.time }.reverse
-    @activity_stats  = Hdo::Charts::Activity.new(@representative.full_name, all_vote_results)
-    
     @vote_results = all_vote_results.paginate(:page => params[:page])
     
     respond_to do |format|
