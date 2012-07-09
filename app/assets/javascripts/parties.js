3cb21bf5b736123394616e8d2328048d958bd129
Jostein Holje <jostein.holje@gmail.com>
2012-07-09 10:31:03 +0200

    Lazy load images in representative carousel.

--- app/assets/javascripts/parties.js
+++ app/assets/javascripts/parties.js
@@ -1,2 +1,16 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+(function (H, $, _) {
+    var carousel = document.getElementById("representative-carousel"),
+        loader;
+    if (!carousel) { return; }
+
+    loader = H.lazyLoadCarouselImages.create(carousel);
+    loader.init();
+
+    // Override carousel.prev, to avoid cycling backwards from the start frame.
+    // Since we don't know which way we are sliding, we don't want to load images for more than one frame per slide.
+    $.fn.carousel.Constructor.prototype.prev = function () {
+        if (this.sliding) { return; }
+        if (loader.getActiveIndex() === 0) { return; }
+        return this.slide('prev');
+    };
+}(HDO, window.jQuery, _));
