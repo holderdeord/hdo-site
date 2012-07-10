9fb0dcdb516962c2085de66351027bc184b2978f
Jonathan Ronen <jonathanronen@hdo>
2012-07-10 18:24:33 +0200

    Changed config.asset.version.

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,6 @@ module Hdo
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
   end
 end
