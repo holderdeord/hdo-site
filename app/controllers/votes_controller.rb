cbec828dc2c8997b1e894727a2ba4249c8e4d747
Jari Bakken <jari.bakken@gmail.com>
2012-07-10 20:04:22 +0200

    Add per_page for votes.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,8 @@ class VotesController < ApplicationController
   caches_page :index, :show
 
   def index
-    @votes = Vote.includes(:issues).paginate(:page => params[:page]).order(:time).reverse_order
+    @votes = Vote.includes(:issues).paginate(:page => params[:page], :per_page => params[:per_page]).
+                                    order(:time).reverse_order
 
     respond_to do |format|
       format.html
