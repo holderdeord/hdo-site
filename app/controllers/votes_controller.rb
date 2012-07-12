569ecb1b07312ba9d3d3f4eeacb840c9af33c113
Jari Bakken <jari.bakken@gmail.com>
2012-07-12 14:41:26 +0200

    Add 'show all' link for Kit

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,14 @@
 class VotesController < ApplicationController
   caches_page :index, :show
 
+  DEFAULT_PER_PAGE = 30
+
   def index
-    @votes = Vote.includes(:issues).paginate(:page => params[:page], :per_page => params[:per_page]).
-                                    order(:time).reverse_order
+    per_page = params[:per_page].to_i > 0 ? params[:per_page] : DEFAULT_PER_PAGE
+
+    @votes = Vote.includes(:issues).
+                  paginate(:page => params[:page], :per_page => per_page).
+                  order(:time).reverse_order
 
     respond_to do |format|
       format.html
