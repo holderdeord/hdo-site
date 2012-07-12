10c760164da61e3ce5a9ad7dfa24a8f042d46412
Jari Bakken <jari.bakken@gmail.com>
2012-07-12 17:47:27 +0200

    Fix table name.

--- db/migrate/20120712153837_add_slug_to_topic.rb
+++ db/migrate/20120712153837_add_slug_to_topic.rb
@@ -1,11 +1,11 @@
 class AddSlugToTopic < ActiveRecord::Migration
   def up
-    add_column :representatives, :slug, :string
-    add_index :representatives, :slug, :unique => true
+    add_column :topics, :slug, :string
+    add_index :topics, :slug, :unique => true
   end
   
   def down
-    remove_index :representatives, :slug
-    remove_column :representatives, :slug
+    remove_index :topics, :slug
+    remove_column :topics, :slug
   end
 end
