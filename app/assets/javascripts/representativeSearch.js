8d20d005c85c6332607aed64e750dfdc77e54fe9
Jostein Holje <jostein.holje@gmail.com>
2012-07-13 14:31:51 +0200

    Representative search box on parties site.  - uses twitter-bootstrap-typeahead extension from https://github.com/tcrosen/twitter-bootstrap-typeahead

---
+++ app/assets/javascripts/representativeSearch.js
@@ -0,0 +1,33 @@
+var HDO = HDO || {};
+
+(function (H, $, _) {
+
+    function clearInput() {
+        $(this).val("");
+    }
+
+    H.representativeSearch = {
+
+        init: function (element, data) {
+            var source = this.parse(data);
+            // uses lib/bootstrap-typeahead.js, extension to regular bootstrap
+            $(element).typeahead({
+                source: source,
+                itemSelected: function (item, val, text) {
+                    document.location = "/representatives/" + val;
+                }
+            });
+
+            $(element).on("focus", clearInput);
+        },
+
+        parse: function (data) {
+            return _.map(data, function (obj) {
+                return {
+                    id: obj.id,
+                    name: [obj.first_name, obj.last_name].join(" ")
+                };
+            });
+        }
+    };
+}(HDO, $, _));
