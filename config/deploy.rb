be6121b46c27d2d4ee73093a0aa0c96cea02b520
Jari Bakken <jari.bakken@gmail.com>
2012-07-17 12:09:57 +0200

    Keep the dragonfly cache between deploys.

--- config/deploy.rb
+++ config/deploy.rb
@@ -78,3 +78,12 @@ namespace :deploy do
     end
   end
 end
+
+namespace :dragonfly do
+  desc "Symlink the Rack::Cache files"
+  task :symlink, :roles => [:app] do
+    run "mkdir -p #{shared_path}/tmp/dragonfly && ln -nfs #{shared_path}/tmp/dragonfly #{release_path}/tmp/dragonfly"
+  end
+end
+
+after 'deploy:update_code', 'dragonfly:symlink'
