66fff1cd566734f098dfe245326be4c05425e468
Jari Bakken <jari.bakken@gmail.com>
2012-07-31 13:36:34 +0200

    Fix typo.

--- app/models/promise.rb
+++ app/models/promise.rb
@@ -6,7 +6,7 @@ class Promise < ActiveRecord::Base
 
   validates_presence_of :source, :body, :party
   validates_uniqueness_of :body, :scope => :party_id
-  validates_length_of :issues, :minimum => 1
+  validates_length_of :categories, :minimum => 1
 
   def general_text
     I18n.t(general? ? 'app.yes' : 'app.no')
