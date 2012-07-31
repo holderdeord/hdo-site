6267efaabf353ce832e22c1316271cd8f71f4e12
Jari Bakken <jari.bakken@gmail.com>
2012-07-31 02:35:25 +0200

    Remove debug logging.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,8 +14,6 @@ module ApplicationHelper
   end
 
   def active_status_for(*what)
-    logger.info [what, controller_name, controller.action_name]
-
     if what.include?(controller_name.to_sym) || what.include?("#{controller_name}_#{controller.action_name}".to_sym)
       'active'
     end  
