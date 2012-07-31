44db44f67de262274dbbefee2df0678ab432093b
Jari Bakken <jari.bakken@gmail.com>
2012-07-31 13:02:27 +0200

    Every Promise must have at least 1 category.

--- app/models/promise.rb
+++ app/models/promise.rb
@@ -1,10 +1,12 @@
 class Promise < ActiveRecord::Base
   belongs_to :party
+
   has_and_belongs_to_many :categories, :order => :name
   has_and_belongs_to_many :topics, :order => :title
 
   validates_presence_of :source, :body, :party
   validates_uniqueness_of :body, :scope => :party_id
+  validates_length_of :issues, :minimum => 1
 
   def general_text
     I18n.t(general? ? 'app.yes' : 'app.no')
