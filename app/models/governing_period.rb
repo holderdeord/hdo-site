0973d0a65c174388326043654c692c8db1a4245d
Jonathan Ronen <yablee@gmail.com>
2012-08-04 18:46:41 +0200

    Added start_date < end_date validation to GoverningPeriod.

--- app/models/governing_period.rb
+++ app/models/governing_period.rb
@@ -3,6 +3,12 @@ class GoverningPeriod < ActiveRecord::Base
   attr_accessible :end_date, :start_date, :party
 
   validates_presence_of :party, :start_date
+  validate :start_date_must_be_before_end_date
+
+  def start_date_must_be_before_end_date
+    errors.add(:start_date, "must be before end date") if
+      end_date && start_date >= end_date
+  end
 
   def include?(date)
     date >= start_date && (end_date == nil || date < end_date)
