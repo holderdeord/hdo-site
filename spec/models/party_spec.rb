3142e80be3f8c55ff4f1009ce7198171e2d21847
Jari Bakken <jari.bakken@gmail.com>
2012-08-04 03:00:10 +0200

    More Party specs.

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -4,9 +4,18 @@ describe Party do
   it "returns its percentage of the parliament" do
     Representative.stub!(:count => 100)
 
-    party = Party.create(:name => "Democratic Party")
+    party = Party.make!(:name => "Democratic Party")
     Representative.make!(:party => party)
 
     party.percent_of_representatives.should == 1
+  end
+
+  it 'is invalid without a name' do
+    Party.make(:name => nil).should_not be_valid
+  end
+
+  it 'is invalid without unique name' do
+    party = Party.make!
+    Party.make(:name => party.name).should_not be_valid
   end
 end
