150d4e3c54959d62217972aefdf1a3877e8998d4
Jonathan Ronen <yablee@gmail.com>
2012-08-04 18:35:05 +0200

    Made form to save multiple governing periods at the same submit on coalition page.

--- app/controllers/coalition_controller.rb
+++ app/controllers/coalition_controller.rb
@@ -6,7 +6,18 @@ class CoalitionController < ApplicationController
   end
 
   def update
+    save_governing_periods
     redirect_to view_coalition_path
+  end
+
+  private
+
+  def save_governing_periods
+    governin_periods_params = params[:governing_periods]
+    governin_periods_params.each do |id,params|
+      governing_period = GoverningPeriod.find id
+      governing_period.update_attributes(params)
+    end
   end
 
 end
