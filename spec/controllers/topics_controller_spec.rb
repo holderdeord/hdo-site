ceb279389df9a236e474fa85ff79ed830fd1a6a5
Jari Bakken <jari.bakken@gmail.com>
2012-08-04 22:45:12 +0200

    Don't use instance variables in spec unless necessary.

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -34,61 +34,62 @@ describe TopicsController do
   end
 
   it "should show votes step when hit next from promises" do
-    @topic = Topic.make!
+    topic = Topic.make!
     session[:topic_step] = 'promises'
 
-    put :update, :topic => topic_params(@topic), :id => @topic.slug
+    put :update, :topic => topic_params(topic), :id => topic.slug
 
     response.should redirect_to edit_topic_step_url( :id => assigns(:topic).slug, :step => 'votes' )
   end
 
   it "should show fields step when hit next from votes" do
-    @topic = Topic.make!
+    topic = Topic.make!
     session[:topic_step] = 'votes'
 
-    put :update, :topic => topic_params(@topic), :id => @topic.slug
+    put :update, :topic => topic_params(topic), :id => topic.slug
 
     response.should redirect_to edit_topic_step_url( :id => assigns(:topic).slug, :step => 'fields' )
   end
 
   it "should show votes step when hit previous from fields" do
-    @topic = Topic.make!
+    topic = Topic.make!
     session[:topic_step] = 'fields'
 
-    put :update, :prev_button => true, :topic => topic_params(@topic), :id => @topic.slug
+    put :update, :prev_button => true, :topic => topic_params(topic), :id => topic.slug
 
     response.should redirect_to edit_topic_step_url( :id => assigns(:topic).slug, :step => 'votes' )
   end
 
   it "should show promises when hit previous from votes" do
-    @topic = Topic.make!
+    topic = Topic.make!
     session[:topic_step] = 'votes'
 
-    put :update, :prev_button => true, :topic => topic_params(@topic), :id => @topic.slug
+    put :update, :prev_button => true, :topic => topic_params(topic), :id => topic.slug
 
     response.should redirect_to edit_topic_step_url( :id => assigns(:topic).slug, :step => 'promises' )
   end
 
   it "should show the categories step when hit prev from promises" do
-    @topic = Topic.make!
+    topic = Topic.make!
     session[:topic_step] = 'promises'
 
-    put :update, :prev_button => true, :topic => topic_params(@topic), :id => @topic.slug
+    put :update, :prev_button => true, :topic => topic_params(topic), :id => topic.slug
 
     response.should redirect_to edit_topic_step_url( :id => assigns(:topic).slug, :step => 'categories' )
   end
 
   it "should save and redirect to topic when hit finish from edit step" do
-    @topic = Topic.make!
+    topic = Topic.make!
     session[:topic_step] = 'votes'
 
-    put :update, :finish_button => true, :topic => topic_params(@topic), :id => @topic.slug
+    put :update, :finish_button => true, :topic => topic_params(topic), :id => topic.slug
 
-    response.should redirect_to topic_path @topic
+    response.should redirect_to topic_path topic
   end
 
   def topic_params(topic)
-    topic.as_json.keep_if { |key| key.in? 'id', 'description', 'title', 'category_ids', 'promise_ids', 'field_ids' }
+    wanted_keys = ['id', 'description', 'title', 'category_ids', 'promise_ids', 'field_ids']
+    topic.as_json.keep_if { |key| key.in? wanted_keys }
   end
 
 end
