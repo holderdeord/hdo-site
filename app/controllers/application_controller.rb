2b866813f6d9f5f03d6b33f93725fca5a8a87337
Arne Hassel <arne.hassel@gmail.com>
2012-08-11 13:10:38 +0200

    Moved elements around in navigation menu. Close #105

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :parties
+
+  private
+
+  def parties
+  	@parties ||= Party.order(:name)
+  end
 end
