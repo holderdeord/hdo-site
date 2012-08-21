935e7cb305dde094be6d7f849c53e18529e4d275
Jonathan Ronen <yablee@gmail.com>
2012-08-21 16:31:19 +0200

    Added (failing) test for vote inferrer to use clustering.

--- spec/lib/hdo/vote_inferrer_spec.rb
+++ spec/lib/hdo/vote_inferrer_spec.rb
@@ -69,6 +69,72 @@ module Hdo
 
       described_class.new([v]).infer!.should == [false]
     end
+
+    describe "with multiple voting sessions in the same day" do
+      before do
+        @rep1 = Representative.make!
+        @rep2 = Representative.make!
+        @rep3 = Representative.make!
+        @rep4 = Representative.make!
+
+
+        @personal_vote_in_other_cluster = Vote.make!(
+          :enacted      => true,
+          :personal     => true,
+          :time         => Time.parse("2012-08-07 18:56"),
+          :vote_results => []
+        )
+
+        @personal_vote_in_other_cluster.vote_results.create! :representative => @rep1, :result => 0
+        @personal_vote_in_other_cluster.vote_results.create! :representative => @rep2, :result => 0
+        @personal_vote_in_other_cluster.vote_results.create! :representative => @rep3, :result => 0
+        @personal_vote_in_other_cluster.vote_results.create! :representative => @rep4, :result => -1
+
+        @personal_vote_in_same_cluster = Vote.make!(
+        :enacted      => true,
+        :personal     => true,
+        :time         => Time.parse("2012-08-07 12:56"),
+        :vote_results => []
+        )
+
+        @personal_vote_in_same_cluster.vote_results.create! :representative => @rep1, :result => 1
+        @personal_vote_in_same_cluster.vote_results.create! :representative => @rep2, :result => 0
+        @personal_vote_in_same_cluster.vote_results.create! :representative => @rep3, :result => -1
+        @personal_vote_in_same_cluster.vote_results.create! :representative => @rep4, :result => -1
+
+        @non_personal_vote = Vote.make!(
+          :enacted      => false,
+          :personal     => false,
+          :time         => Time.parse("2012-08-07 13:00"),
+          :vote_results => []
+        )
+
+        subject.infer!.should == [true]
+
+        @non_personal_vote.reload
+      end
+
+      it "infers the representatives from the personal vote of the same cluster" do
+
+        # why can't we just do :representative => rep1 here?
+        @non_personal_vote.vote_results.where(:representative_id => @rep1.id).first.result.should == -1
+        @non_personal_vote.vote_results.where(:representative_id => @rep2.id).first.result.should == 0
+        @non_personal_vote.vote_results.where(:representative_id => @rep3.id).first.result.should == -1
+        @non_personal_vote.vote_results.where(:representative_id => @rep4.id).first.result.should == -1
+      end
+
+      it "infers that the non-personal fote has 3 votes against" do
+        @non_personal_vote.against_count.should == 3
+      end
+
+      it "should infer 1 absent voter" do
+        @non_personal_vote.absent_count.should == 1
+      end
+
+      it "should infer 0 for votes" do
+        @non_personal_vote.for_count.should == 0
+      end
+    end
   end
 
 end
