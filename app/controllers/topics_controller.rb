beb3fa634890ae33cdca2ddf2fa223a52c037ea9
Jonathan Ronen <yablee@gmail.com>
2012-08-21 19:31:35 +0200

    Added other issues to topic#show.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -19,6 +19,8 @@ class TopicsController < ApplicationController
   def show
     @issues = @topic.issues
 
+    @other_issues = Issue.all - @issues
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @topic }
