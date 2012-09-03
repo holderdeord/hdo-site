60bcbe5254b61b0caf5633418cbfac960385afaf
Jari Bakken <jari.bakken@gmail.com>
2012-09-04 00:01:38 +0200

    Ensure correct require order.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,12 @@ Spork.prefork do
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+  # ensure correct require order
+  require Rails.root.join('spec/support/pages/page')
+  require Rails.root.join('spec/support/pages/menu')
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| p f; require f }
 
   RSpec.configure do |config|
     config.color = $stdout.tty?
