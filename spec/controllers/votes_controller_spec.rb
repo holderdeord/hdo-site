498c87380d219c01fec034efd301c746136231bc
Jari Bakken <jari.bakken@gmail.com>
2012-09-05 01:29:24 +0200

    Remove old spec.

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -16,12 +16,4 @@ describe VotesController do
 
     assigns(:vote).should eq @vote
   end
-
-  it "should be not be paginated when accessing votes#all" do
-    40.times { Vote.make! }
-
-    get :all
-
-    Nokogiri::HTML.parse(response.body).inner_text.strip.should =~ /Viser alle/
-  end
 end
