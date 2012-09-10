390a537c9a62dc3b48f281c23f1388dfc0c2cf81
Kat Aquino <katfaquino@gmail.com>
2012-09-10 13:32:38 +0200

    changed tab image

---
+++ app/assets/javascripts/feedback.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('.slide-out-div').tabSlideOut({
+    tabHandle: '.handle',                     //class of the element that will become your tab
+    pathToTabImage: '/assets/feedback.png', //path to the image for the tab //Optionally can be set  css
+    imageHeight: '135px',                     //height of tab image           //Optionally can be set using css
+    imageWidth: '20px',                       //width of tab image            //Optionally can be set using css
+    tabLocation: 'right',                      //side of screen where tab lives, top, right, bottom, or left
+    speed: 300,                               //speed of animation
+    action: 'click',                          //options: 'click' or 'hover', action to trigger animation
+    topPos: '500px',                          //position from the top/ use if tabLocation is left or right
+    leftPos: '20px',                          //position from left/ use if tabLocation is bottom or top
+    fixedPosition: true                      //options: true makes it stick(fixed position) on scroll
+  });
+});
