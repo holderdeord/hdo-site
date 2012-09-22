6d5958e163ada10cbe1fe4fa3989fcbcecc4e74e
Jari Bakken <jari.bakken@gmail.com>
2012-09-22 12:39:02 +0200

    Change issue description from :string to :text.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120910161902) do
+ActiveRecord::Schema.define(:version => 20120922102553) do
 
   create_table "categories", :force => true do |t|
     t.string   "external_id"
@@ -85,7 +85,7 @@ ActiveRecord::Schema.define(:version => 20120910161902) do
 
   create_table "issues", :force => true do |t|
     t.string   "title"
-    t.string   "description"
+    t.text     "description"
     t.datetime "created_at",                            :null => false
     t.datetime "updated_at",                            :null => false
     t.string   "slug"
