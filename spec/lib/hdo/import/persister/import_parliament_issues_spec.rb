794a0ac57981535a24c748ab6001c5e274afd4dc
Jari Bakken <jari.bakken@gmail.com>
2012-09-25 20:11:59 +0200

    Specs for Persister#import_parliament_issue.

--- spec/lib/hdo/import/persister/import_parliament_issues_spec.rb
+++ spec/lib/hdo/import/persister/import_parliament_issues_spec.rb
@@ -7,8 +7,51 @@ module Hdo
 
         include_context :persister
 
-        it 'imports a parliament issue'
-        it 'updates an existing parliament issue based on external id'
+        def setup_parliament_issue(issue)
+          Committee.find_or_create_by_name(issue.committee)
+          issue.categories.each { |c| Category.find_or_create_by_name(c) }
+        end
+
+        it 'imports a parliament issue' do
+          parliament_issue = StortingImporter::ParliamentIssue.example
+          setup_parliament_issue(parliament_issue)
+
+          persister.import_parliament_issue parliament_issue
+          ParliamentIssue.count.should == 1
+        end
+
+        it 'imports multiple parliament issues' do
+          first  = StortingImporter::ParliamentIssue.example
+          second = StortingImporter::ParliamentIssue.from_hash(
+            StortingImporter::ParliamentIssue.example.to_hash.merge('externalId' => '1234', 'status' => 'behandlet')
+          )
+
+          setup_parliament_issue(first)
+          setup_parliament_issue(second)
+
+          persister.import_parliament_issues [first, second]
+
+          ParliamentIssue.count.should == 2
+        end
+
+        it 'updates an existing parliament issue based on external id' do
+          parliament_issue = StortingImporter::ParliamentIssue.example
+          setup_parliament_issue(parliament_issue)
+
+          persister.import_parliament_issue parliament_issue
+          ParliamentIssue.count.should == 1
+
+          update = StortingImporter::ParliamentIssue.from_hash(
+            parliament_issue.to_hash.merge('status' => 'behandlet')
+          )
+
+          persister.import_parliament_issue update
+          ParliamentIssue.count.should == 1
+
+          pi = ParliamentIssue.first
+          pi.external_id.should == parliament_issue.external_id
+          pi.status.should == update.status
+        end
 
       end
     end
