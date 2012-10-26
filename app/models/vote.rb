d305875684ced36fcde9f3f76790954a457192b4
Jonathan Ronen <yablee@gmail.com>
2012-10-26 18:58:24 +0200

    Add correct prop types keys and translations.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -6,8 +6,8 @@ class Vote < ActiveRecord::Base
                   :enacted, :personal, :subject, :time, :external_id,
                   :proposition_type
 
-  PROPOSITION_TYPES = %w[representative_proposition budget_amendment yada_yada]
-  validates_inclusion_of :proposition_type, in: [PROPOSITION_TYPES, nil]
+  PROPOSITION_TYPES = %w[parliamentary_report proposal_attached_to_the_minutes representative_proposition opposition_proposition_budget opposition_proposition_parliamentary_report opposition_proposition_legislation legislation national_budget_financial_proposition national_budget_committee_proposition revised_national_budget rebalanced_national_budget]
+  validates_inclusion_of :proposition_type, in: [PROPOSITION_TYPES, nil, ''].flatten
 
   has_and_belongs_to_many :parliament_issues, uniq: true
   validates_length_of     :parliament_issues, minimum: 1
