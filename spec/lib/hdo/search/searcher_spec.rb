2b462719601fdc3f121ca8eaf9aca42ae20929a8
Jari Bakken <jari.bakken@gmail.com>
2012-10-27 13:33:21 +0200

    Add pending specs for search.

---
+++ spec/lib/hdo/search/searcher_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+module Hdo
+  module Search
+
+    describe Searcher do
+      it 'should be tested'
+    end
+
+  end
+end
---
+++ spec/lib/hdo/search/settings_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+module Hdo
+  module Search
+
+    describe Settings do
+      it 'should be tested'
+    end
+
+  end
+end
