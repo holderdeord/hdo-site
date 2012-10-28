5dc463e0ac31c76a85bc37e4a6d7086977448b93
Jari Bakken <jari.bakken@gmail.com>
2012-10-29 00:01:37 +0100

    Fix order of result groups.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,21 @@
 class SearchController < ApplicationController
+  TYPE_ORDER = %w[
+    issue
+    topic
+    representative
+    parliament_issue
+    proposition
+    promise
+  ]
+
   def all
     response = Hdo::Search::Searcher.new(params[:query]).all
     @results = []
 
     if response.success?
-      @results = response.results.group_by { |e| e.type }
-
-      respond_to do |format|
-        format.html
-      end
+      @results = response.results.
+                          group_by { |e| e.type }.
+                          sort_by { |t, _| TYPE_ORDER.index(t) || 10 }
     else
       flash.alert = t('app.errors.search')
     end
