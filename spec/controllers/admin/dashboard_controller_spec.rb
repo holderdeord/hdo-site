738525f21655556ef762c3422f4754e1fec6cf19
Jari Bakken <jari.bakken@gmail.com>
2012-11-02 19:12:58 +0100

    Add spec for DashboardController.

--- spec/controllers/admin/dashboard_controller_spec.rb
+++ spec/controllers/admin/dashboard_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Admin::DashboardController do
+  let(:user) { User.make! }
+
+  it 'renders the dashboard' do
+    sign_in user
+
+    get :index
+    response.should have_rendered(:index)
+  end
+
+  it 'does not render the dashboard if unauthorized' do
+    get :index
+    response.should redirect_to(new_user_session_path)
+  end
 
 end
