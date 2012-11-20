e3b38faf1b8acb3b1a59e405388386317f319769
Jari Bakken <jari@holderdeord.no>
2012-11-20 18:36:57 +0100

    Set app name + env as index prefix.

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -1,12 +1,13 @@
 require 'tire/http/clients/faraday'
 
+Tire::Model::Search.index_prefix "hdo_#{Rails.env}"
+
 Tire.configure do
   url    ENV['BONSAI_URL'] || AppConfig.elasticsearch_url
   logger Rails.root.join("log/tire_#{Rails.env}.log")
 
   Tire::HTTP::Client::Faraday.faraday_middleware = ->(fd) { fd.adapter :net_http_persistent }
   client Tire::HTTP::Client::Faraday
-
 end
 
 TireSettings = Hdo::Search::Settings
