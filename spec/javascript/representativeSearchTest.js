bcb3e4b70700f5aa49084ab31fd8155af327104a
Jostein Holje <jostein.holje@gmail.com>
2012-11-25 14:00:24 +0100

    Update representative search to use correct version of typeahead.

--- spec/javascript/representativeSearchTest.js
+++ spec/javascript/representativeSearchTest.js
@@ -1,28 +1,44 @@
-/* global HDO*/
+/*global cull, HDO */
 
-(function (H) {
-    buster.testCase('Representative Search', {
-        "should build search data from model": function () {
-            var dataFromServer = [{
-                "created_at":"2012-06-30T10:12:37Z",
-                "date_of_birth":"1967-02-20T23:00:00Z",
-                "date_of_death":null,
-                "district_id":1,
-                "external_id":"MAAA",
-                "first_name":"Marianne",
-                "id":110,
-                "last_name":"Aasen",
-                "slug": "maaa",
-                "party_id":1,
-                "updated_at":"2012-06-30T10:12:37Z"
-            }];
+(function (H, cull) {
+  buster.testCase('Representative Search', {
+    setUp: function () {
+      this.dataFromServer = [{
+        "created_at":"2012-06-30T10:12:37Z",
+        "date_of_birth":"1967-02-20T23:00:00Z",
+        "date_of_death":null,
+        "district_id":1,
+        "external_id":"MAAA",
+        "first_name":"Marianne",
+        "id":110,
+        "last_name":"Aasen",
+        "slug": "maaa",
+        "party_id":1,
+        "updated_at":"2012-06-30T10:12:37Z"
+      }];
 
-            var result = H.representativeSearch.parse(dataFromServer);
-            assert.equals(result, [{
-                id: "maaa",
-                name: "Marianne Aasen"
-            }]);
-        }
+      this.searcher = H.representativeSearch.create({
+        data: this.dataFromServer
+      });
+    },
 
-    });
-}(HDO));
+    "should build search data from model": function () {
+      assert.equals(this.searcher.mappedData, {
+        "Marianne Aasen": "maaa"
+      });
+    },
+
+    "should return source data": function () {
+      assert.equals(this.searcher.getSource(), ["Marianne Aasen"]);
+    },
+
+    "should redirect on update": function () {
+      var listener = this.spy();
+      H.redirect = listener;
+
+      this.searcher.updater("Marianne Aasen");
+      assert.calledOnceWith(listener, "/representatives/maaa");
+    }
+
+  });
+}(HDO, cull));
