7e80679bb5c1d08a4854e834750f3c452b5bc9ef
Jari Bakken <jari@holderdeord.no>
2012-11-26 18:27:15 +0100

    Try static Dragonfly secret to resolve image issues.

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -3,5 +3,5 @@ require 'dragonfly/rails/images'
 Dragonfly[:images].configure do |c|
   c.allow_fetch_file         = true
   c.protect_from_dos_attacks = true
-  c.secret                   = Base64.strict_encode64(Random.new.bytes(32))
+  c.secret                   = "YTHUqMUdv5N18fDvn64Yfq8YMkOHbgJZrVDgUmYs88Q="
 end
