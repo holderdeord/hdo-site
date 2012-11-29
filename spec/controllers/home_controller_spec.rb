766f3bd9b047b161485c00ae75011f27cc92446e
Jari Bakken <jari.bakken@gmail.com>
2012-11-30 00:19:44 +0100

    Add spec to make sure only published issues are shown on the front page.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
 
 describe HomeController do
+  context 'issues' do
+    it 'loads only published issues' do
+      shown     = Issue.make! status: 'published'
+      not_shown = Issue.make! status: 'shelved'
+
+      get :index
+
+      assigns(:issues).should == [shown]
+    end
+  end
+
   context 'with rendered views' do
     render_views
 
