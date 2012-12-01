f0ef92bb366ff6851a48f2fe075f3f1bafb9dbdd
Jari Bakken <jari.bakken@gmail.com>
2012-12-01 12:26:21 +0100

    Don't rely on Google Code for HTML5 shim.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Hdo::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( some/file.css )
+  config.assets.precompile += %w( conditional/html5.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
