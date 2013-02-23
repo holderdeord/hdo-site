4a36454fecedf99dc74d5b6ee3f5ff3b296e87fa
Jonathan Ronen <yablee@gmail.com>
2013-02-23 20:15:33 +0100

    Improve the symbol->class resolution in Hdo::Search::Index

--- lib/hdo/search/index.rb
+++ lib/hdo/search/index.rb
@@ -5,13 +5,15 @@ module Hdo
       def update_index_on_change_of(*args)
         singular = self.to_s.downcase
         plural   = singular.pluralize
-        classes  = args.map { |a| a.to_s.singularize.capitalize.constantize }
+        classes  = args.map { |a| a.to_s.classify.constantize }
 
         classes.each do |clazz|
           if clazz.method_defined? singular
             clazz.send :after_save, "#{singular}.tire.update_index"
           elsif clazz.method_defined? plural
             clazz.send :after_save, "#{plural}.each {|e| e.tire.update_index}"
+          else
+            raise "Model #{self} has no relation #{clazz}."
           end
 
         end
