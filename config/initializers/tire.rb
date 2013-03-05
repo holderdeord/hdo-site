fb724066a131959f1e8a55a8dd43fceed0a3b60c
Jari Bakken <jari@holderdeord.no>
2013-03-05 20:16:04 +0100

    Use ELASTICSEARCH_URL if set

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -3,7 +3,7 @@ require 'tire/http/clients/faraday'
 Tire::Model::Search.index_prefix "hdo_#{Rails.env}"
 
 Tire.configure do
-  url    ENV['BONSAI_URL'] || AppConfig.elasticsearch_url
+  url    AppConfig.elasticsearch_url
   logger Rails.root.join("log/tire_#{Rails.env}.log")
 
   Tire::HTTP::Client::Faraday.faraday_middleware = ->(fd) { fd.adapter :net_http_persistent }
