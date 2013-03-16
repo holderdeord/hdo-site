b659fd5541a619e5a4a9fe44f5ad78bf4961e130
Jari Bakken <jari@holderdeord.no>
2013-03-17 00:08:12 +0100

    Set RAILS_CACHE_ID to the current revision if available.
    This will ensure unique etags/cache keys.

--- config/initializers/cache.rb
+++ config/initializers/cache.rb
@@ -1,3 +1,9 @@
 unless Rails.env.production?
   Rails.cache.logger = Rails.logger
 end
+
+revision = Rails.root.join('REVISION')
+
+if revision.exist?
+  ENV['RAILS_CACHE_ID'] = revision.read[0,7]
+end
