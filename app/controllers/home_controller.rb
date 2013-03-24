703f1425331fa281cbe9b4a5198927da2e6501f1
Kat Aquino <katfaquino@gmail.com>
2013-03-24 18:09:31 +0100

    Changed page heading and added old members

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -102,7 +102,12 @@ class HomeController < ApplicationController
       Person.new('Einar Sundin'),
       Person.new('Eirik Swensen'),
       Person.new('Ole Martin Volle'),
-      Person.new('Guro Øistensen')
+      Person.new('Guro Øistensen'),
+      Person.new('Jan Olav Ryfetten'),
+      Person.new('Frode Hiorth'),
+      Person.new('Jostein Holje'),
+      Person.new('Markus Krüger'),
+      Person.new('Salve Nilsen')
      ]
   end
 
