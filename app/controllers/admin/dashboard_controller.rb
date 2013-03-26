bb8fc8dea90d070a3266ed16c4ed872c12e0c1d1
Jari Bakken <jari@holderdeord.no>
2013-03-26 23:28:57 +0100

    Show some stats in /admin - see #456.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,13 @@
 class Admin::DashboardController < AdminController
   def index
-    @votes_by_date = Vote.includes(:parliament_issues).latest(30).group_by { |v| v.time.to_date }
+    @votes_by_date               = Vote.includes(:parliament_issues).latest(30).group_by { |v| v.time.to_date }
     @parliament_issues_by_status = ParliamentIssue.latest(10).group_by(&:status_text)
-    @issues_by_status = Issue.latest(10).group_by(&:status_text)
-    @pending_questions = Question.pending
+    @issues_by_status            = Issue.latest(10).group_by(&:status_text)
+    @pending_questions           = Question.pending
+
+    published = Issue.published
+
+    @issue_vote_percentage       = published.flat_map(&:vote_ids).uniq.size * 100 / Vote.count
+    @issue_promise_percentage    = published.flat_map(&:promise_ids).uniq.size * 100 / Promise.count
   end
 end
