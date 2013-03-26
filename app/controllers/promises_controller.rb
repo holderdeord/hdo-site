eb325dcb9a0ae59c696f0ae34e45cc5d651240a5
Jari Bakken <jari@holderdeord.no>
2013-03-26 23:17:58 +0100

    Fix page header on /promises

--- app/controllers/promises_controller.rb
+++ app/controllers/promises_controller.rb
@@ -4,7 +4,6 @@ class PromisesController < ApplicationController
   def index
     @categories = Category.where(:main => true).includes(:children)
     @parties    = Party.order(:name)
-    @page_title = Promise.model_name.human(count: 2).capitalize
  end
 
   protected
