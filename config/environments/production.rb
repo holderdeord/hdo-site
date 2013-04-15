62b78d7935f7d060de8ac910a3dd57dd05a21a5b
Jari Bakken <jari@holderdeord.no>
2013-04-16 00:23:22 +0200

    Remove invalid asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Hdo::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( conditional/html5.js widgets/widgets.js)
+  config.assets.precompile += %w( conditional/html5.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
