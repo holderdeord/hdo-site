89feffd79cc3f2113039ebf68ecf1c0583b9a0ab
Jari Bakken <jari@holderdeord.no>
2013-04-21 23:50:04 +0200

    Optimize PromiseController + code style cleanup.

--- app/controllers/promises_controller.rb
+++ app/controllers/promises_controller.rb
@@ -4,45 +4,29 @@ class PromisesController < ApplicationController
   DEFAULT_PER_PAGE = 30
 
   def index
-    @categories = Category.where(:main => true).includes(:children)
-    @category = @categories.find(params[:category_id]) if params[:category_id].present?
+    @categories  = Category.where(main: true).includes(:children)
+    @category    = @categories.find(params[:category_id]) if params[:category_id].present?
     @subcategory = Category.find(params[:subcategory_id]) if params[:subcategory_id].present?
-    @parties    = Party.order(:name)
-    @party = @parties.find(params[:party_slug]) if params[:party_slug].present?
-    render_promises_index()
+    @parties     = Party.order(:name)
+    @party       = @parties.find(params[:party_slug]) if params[:party_slug].present?
+
+    if @subcategory
+      @promises = Category.find(@subcategory.id).promises
+    elsif @category
+      @promises = @category.all_promises
+    else
+      @promises = Promise.includes(:parties).order('date')
+    end
+
+    if @party
+      @promises = @promises.joins(:parties).where('parties.id' => @party.id)
+    end
+
+    @promises = @promises.paginate(page: params[:page], per_page: DEFAULT_PER_PAGE)
   end
 
   def show
     @promise = Promise.find(params[:id])
   end
 
-  protected
-
-  def find_promise
-    @promise = Promise.find(params[:id])
-  end
-
-  private
-
-  def render_promises_index(opts = {})
-    @promises = Promise.includes(:issues, :parties).all(:order => 'date')
-
-    if @subcategory
-      @promises = Category.find(@subcategory.id).promises
-    elsif @category
-      @promises = @category.all_promises
-    end
-
-    if @party
-      @promises = @promises.select { |p| p.parties.include? @party }
-    end
-
-    @promises = @promises.paginate(:page => params[:page], :per_page => DEFAULT_PER_PAGE)
-
-    respond_to do |format|
-      format.html { render :index }
-      format.json { render json: @promises }
-      format.xml  { render xml:  @promises }
-    end
-  end
 end
