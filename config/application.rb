e1a3abf1fdc8c9cb663e5ec6f4f1708576700c05
Henrik Helmers <helmers@me.com>
2013-05-01 20:11:55 +0200

    Adding respond.js conditional (IE8 and below) for widgets.

--- config/application.rb
+++ config/application.rb
@@ -74,7 +74,7 @@ module Hdo
     config.assets.initialize_on_precompile = false
 
     # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-    config.assets.precompile += %w( conditional/html5.js widgets/widgets.css widgets/frame.js)
+    config.assets.precompile += %w( conditional/html5.js conditional/respond.js widgets/widgets.css widgets/frame.js)
 
     # allow cors from other subdomains
     config.middleware.use Rack::Cors do
