409fb26540b17cffcfde8e5652e161b1c16157e7
Jari Bakken <jari@holderdeord.no>
2013-05-05 23:38:08 +0200

    Widgets: fixing link TODOs

--- app/helpers/widgets_helper.rb
+++ app/helpers/widgets_helper.rb
@@ -6,4 +6,8 @@ module WidgetsHelper
   def slug_for_parties(parties)
     parties.size == 1 ? parties.first.slug : ''
   end
+
+  def links_for_parties(parties)
+    parties.map { |party| link_to party.name, party }.to_sentence
+  end
 end
