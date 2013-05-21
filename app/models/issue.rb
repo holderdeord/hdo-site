dc6d0faa7d14e0db6de1567cf41e09c3b9d60728
Jari Bakken <jari@holderdeord.no>
2013-05-21 21:32:16 +0200

    Remove Issue's has_many callbacks
    This is handled by IssueUpdater, which touches the issue
    on any change, including assocations.

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -47,14 +47,8 @@ class Issue < ActiveRecord::Base
   belongs_to :editor, class_name: 'User'
 
   has_many :party_comments, dependent: :destroy
-
-  has_many :vote_connections, dependent:     :destroy,
-                              before_add:    :clear_stats_cache,
-                              before_remove: :clear_stats_cache
-
-  has_many :promise_connections, dependent:     :destroy,
-                                 before_add:    :clear_stats_cache,
-                                 before_remove: :clear_stats_cache
+  has_many :vote_connections, dependent: :destroy
+  has_many :promise_connections, dependent: :destroy
 
   has_many :votes,    through: :vote_connections,    order: :time
   has_many :promises, through: :promise_connections
