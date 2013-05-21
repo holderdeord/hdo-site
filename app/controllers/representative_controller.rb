cc938089caadb27d436a35eaa712af3b3f739992
Jonathan Ronen <yablee@gmail.com>
2013-05-21 23:16:01 +0200

    Order questions in representative's dashboard, and fix partials

--- app/controllers/representative_controller.rb
+++ app/controllers/representative_controller.rb
@@ -4,11 +4,12 @@ class RepresentativeController < ApplicationController
 
   def index
     @questions = Question.approved.where("representative_id = ?", current_representative)
+    @questions.sort_by! { |q| question_sorter q }
   end
 
   def show_question
     @question = Question.approved.find(params[:id])
-    @answer = Answer.new(question: @question, representative: @question.representative)
+    @answer   = Answer.new(question: @question, representative: @question.representative)
   end
 
   def create_answer
@@ -22,6 +23,20 @@ class RepresentativeController < ApplicationController
       else
         redirect_to representative_root_path, alert: @answer.errors.full_messages.to_sentence
       end
+    end
+  end
+
+  private
+
+  def question_sorter(question)
+    if question.answer.nil?
+      0
+    elsif question.answer.rejected?
+      1
+    elsif question.answer.pending?
+      2
+    else
+      3
     end
   end
 
