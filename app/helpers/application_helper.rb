a0f530c06b836a03d24c1c1702f7103dd6cbc927
Jari Bakken <jari@holderdeord.no>
2013-05-25 21:07:55 +0200

    Minor fixes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -61,7 +61,7 @@ module ApplicationHelper
 
   def gravatar_url(email)
     default = asset_url("representatives/unknown.jpg")
-    "//gravatar.com/avatar/#{Digest::MD5.hexdigest email}?s=300&d=#{default}"
+    "//gravatar.com/avatar/#{Digest::MD5.hexdigest email}?s=300&d=#{URI.encode default}"
   end
 
   def page_type(page_type = nil)
