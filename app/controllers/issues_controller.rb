753ccd8dfc615af57bf4ce1f0aead01525b69f83
Jari Bakken <jari@holderdeord.no>
2013-05-27 11:41:32 +0200

    Add JSON representation for /issues/ids.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -12,6 +12,13 @@ class IssuesController < ApplicationController
 
   def ids
     @issues = Issue.published.order(:title)
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: @issues.map { |i| i.as_json(only: [:id, :title]).merge(url: issue_url(i)) }
+      end
+    end
   end
 
   def show
