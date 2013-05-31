6857f81d722ff747f91e3c64164a9735f0e03b14
Jonathan Ronen <yablee@gmail.com>
2013-05-31 20:45:45 +0200

    Add default bcc and reply_to in our mailer

--- lib/hdo/mailer.rb
+++ lib/hdo/mailer.rb
@@ -1,5 +1,8 @@
 module Hdo
   class Mailer < Devise::Mailer
+    default bcc:      'test@holderdeord.no',
+            reply_to: 'kontakt@holderdeord.no'
+
     def devise_mail(record, action, opts={})
       initialize_from_record(record)
       mail(headers_for(action, opts)) do |format|
