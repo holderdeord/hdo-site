b4a1b2d2278a11d179bb3ad2a4f7f500451b2c02
Jonathan Ronen <yablee@gmail.com>
2013-06-02 21:50:14 +0200

    Remove redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < Devise::SessionsController
   include Hdo::DeviseControllerHelper
   hdo_force_ssl
-  before_filter :enforce_devise_user_scope, only: :new
 
   def create
     setup_params_for_hdo_resource
@@ -9,12 +8,5 @@ class SessionsController < Devise::SessionsController
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(hdo_resource_name, resource)
     respond_with resource, :location => after_sign_in_path_for(resource)
-  end
-
-  private
-
-  def enforce_devise_user_scope
-    flash.keep
-    redirect_to new_user_session_path unless request.path == new_user_session_path
   end
 end
