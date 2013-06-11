44bc04eca866b0dfb76b623926668518d8bfe17a
Jonathan Ronen <yablee@gmail.com>
2013-06-11 18:39:31 +0200

    Widget: Sort by party name, not party order.

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -41,7 +41,7 @@ class WidgetsController < ApplicationController
 
     if params[:promises]
       params[:promises].each do |title, ids|
-        @promise_groups << [title, Promise.includes(:parties).find(ids.split(',')).sort_by { |p| p.parties.first }]
+        @promise_groups << [title, Promise.includes(:parties).find(ids.split(',')).sort_by { |p| p.parties.first.name }]
       end
     end
   end
