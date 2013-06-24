d318bc11bd8afa9f474a664180e46cca9d7d28dc
Jonathan Ronen <yablee@gmail.com>
2013-06-25 01:57:05 +0200

    Add color for opted out reps in admin/representatives. Add legend.

--- app/helpers/admin/representatives_helper.rb
+++ app/helpers/admin/representatives_helper.rb
@@ -2,10 +2,12 @@ module Admin::RepresentativesHelper
   def row_class_for_admin_representative(rep)
     if rep.confirmed?
       "success"
+    elsif rep.opted_out?
+      "error"
     elsif rep.confirmation_sent_at
       "warning"
     else
-      "error"
+      "info"
     end
   end
 end
