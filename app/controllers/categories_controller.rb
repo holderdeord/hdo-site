7a89b924d58c69b197474f65289bfad5aab4c0bb
Jari Bakken <jari@holderdeord.no>
2013-08-05 22:51:26 +0200

    Simplify categories#show + add parliament period.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,16 @@
 class CategoriesController < ApplicationController
-  hdo_caches_page :index, :show, :promises
+  hdo_caches_page :index, :show
 
   def index
     @categories = Category.all_with_children
   end
 
   def show
-    @category = Category.includes(:parliament_issues).find(params[:id])
+    @category = Category.includes(
+      :parliament_issues,
+      promises: [:parliament_period, :parties, :categories]
+    ).find(params[:id])
+
     fresh_when @category, public: can_cache?
   end
 
