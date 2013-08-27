f9e33414cf8d31cfa5b1686e73f436d90a1b75bc
Jari Bakken <jari@holderdeord.no>
2013-08-28 01:42:08 +0200

    Install the latest npm to work around bugs

--- script/travis/npm_bootstrap.sh
+++ script/travis/npm_bootstrap.sh
@@ -2,5 +2,8 @@
 
 set -e
 
-npm cache clear
-npm install -g buster sinon@1.6.0 autolint || (sleep 5 && npm install -g buster sinon@1.6.0 autolint)
+function setup () {
+  npm install -g npm && npm install -g buster sinon@1.6.0 autolint
+}
+
+setup || (sleep 5 && setup)
