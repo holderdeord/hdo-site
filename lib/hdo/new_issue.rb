531592d3fc2137e74d58bdbadc241f0c5777208f
Jari Bakken <jari@holderdeord.no>
2013-10-10 01:05:46 +0200

    Starting over on issue timeline

--- lib/hdo/new_issue.rb
+++ lib/hdo/new_issue.rb
@@ -6,26 +6,18 @@ module Hdo
   class NewIssue
     extend Forwardable
 
-    def_delegators :@data, :title,
+    def_delegators :@issue, :title,
                            :description,
                            :updated_at,
                            :tags
 
 
     def self.from_issue(issue)
-      new DeepStruct.new(issue.as_json(
-        include: [
-           { :tags => {methods: :slug}},
-           { :vote_connections    => {:include => {:vote => {methods: :stats}}}},
-           { :promise_connections => {:include => {:promise => {:include => :parties}}}},
-           { :valence_issue_explanations => {:include => :parties}}
-        ]
-      ))
+      new(issue)
     end
 
-    def initialize(data = DeepStruct.load_issue)
-      @data = data
-      @issue = Issue.find(@data.id)
+    def initialize(issue)
+      @issue = issue
     end
 
     def explanation
@@ -39,87 +31,21 @@ module Hdo
     end
 
     def vote_connections
-      data.vote_connections.sort_by { |e| e.vote.time }.reverse.map { |vc| FakeVote.new(vc) }
+      @issue.vote_connections.sort_by { |e| e.vote.time }.reverse
     end
 
-    def positions
-      @positions ||= (
-        data.valence_issue_explanations.map do |vie|
-          DeepStruct.new(
-            'title' => vie.title,
-            'explanation' => vie.explanation,
-            'parties' => vie.parties.map do |p|
-              party = party_named(p.name)
-              DeepStruct.new(
-                'name'                => p.name,
-                'accountability_text' => issue.accountability.text_for(party, name: ''),
-                'position_text'       => vie.explanation,
-                'position_title'      => vie.title,
-                'promises'            => data.promise_connections.select { |pc| pc.promise.parties.any? { |e| e.name == p.name } }.map(&:promise),
-                'small_logo'          => party.logo.versions[:small],
-                'medium_logo'         => party.logo.versions[:medium],
-              )
-            end
-          )
-        end
-      )
-    end
+    def periods
+      periods = @issue.vote_connections.map { |e| e.vote.parliament_period }.uniq
 
-    def parties
-      @parties ||= (
-        party_names = data.vote_connections.flat_map { |e| e.vote.stats.as_json[:parties].keys }.uniq.map(&:to_s)
-        party_names.sort.map do |pn|
-          party = party_named(pn)
-          valence_issue_explanation = data.valence_issue_explanations.find { |vie| vie.parties.map(&:name).include?(pn) }
-
-          DeepStruct.new(
-            'name'                => pn,
-            'accountability_text' => issue.accountability.text_for(party, name: ''),
-            'position_text'       => valence_issue_explanation.explanation,
-            'position_title'      => valence_issue_explanation.title,
-            'promises'            => data.promise_connections.select { |pc| pc.promise.parties.any? { |e| e.name == pn } }.map(&:promise),
-            'small_logo'          => party.logo.versions[:small],
-            'medium_logo'         => party.logo.versions[:medium],
-          )
-        end
-      )
-    end
-
-    private
-
-    attr_reader :data, :issue
-
-    def party_named(name)
-      Party.find_by_name!(name)
-    end
-
-    class FakeVote
-      def initialize(conn)
-        @conn = conn
-      end
-
-      def time
-        I18n.l @conn.vote.time, format: :text
-      end
-
-      def title
-        @conn.title
-      end
-
-      def enacted?
-        @conn.vote.enacted
-      end
-
-      def comment
-        @conn.comment
-      end
-
-      def parties_for
-        @conn.vote.stats.as_json[:parties].select { |name, e| e[:for].to_i > e[:against].to_i }.map(&:first).sort
-      end
-
-      def parties_against
-        @conn.vote.stats.as_json[:parties].select { |name, e| e[:for].to_i < e[:against].to_i }.map(&:first).sort
+      periods.map do |period|
+        OpenStruct.new(name: period.name, events: [
+          OpenStruct.new(name: "Event 1"),
+          OpenStruct.new(name: "Event 2"),
+          OpenStruct.new(name: "Event 3"),
+        ], positions: [
+          OpenStruct.new(title: "Bygge flere"),
+          OpenStruct.new(title: "Bygge færre")
+        ])
       end
     end
 
