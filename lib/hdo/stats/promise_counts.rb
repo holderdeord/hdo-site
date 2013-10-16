395f5d76dd14f5db2dec9c6ffa29408ec1d7ae73
Jari Bakken <jari@holderdeord.no>
2013-10-16 21:00:18 +0200

    PromiseCounter: add ability to see all categories as well
    [ci skip]

--- lib/hdo/stats/promise_counts.rb
+++ lib/hdo/stats/promise_counts.rb
@@ -7,24 +7,29 @@ module Hdo
         @promises = promises || ParliamentPeriod.current.promises
       end
 
-      def by_main_category
+      def by_category(only_main)
         result = Hash.new { |hash, category| hash[category] = [] }
 
         @promises.each do |promise|
-          cats = promise.categories.map { |cat| cat.main? ? cat : cat.parent }.uniq
-          cats.each { |c| result[c] << promise  }
+          if only_main
+            cats = promise.categories.map { |cat| cat.main? ? cat : cat.parent }.uniq
+          else
+            cats = promise.categories
+          end
+
+          cats.each { |c| result[c] << promise }
         end
 
         result
       end
 
-      def csv
+      def csv(only_main = true)
         parties = Party.order(:name)
 
         CSV.generate do |csv|
           csv << ["", "Snitt", *parties.map(&:external_id)]
 
-          by_main_category.each do |category, promises|
+          by_category(only_main).each do |category, promises|
             by_party_counts = Hash.new(0)
 
             promises.each do |promise|
@@ -38,9 +43,15 @@ module Hdo
               avg = 0
             end
 
-            csv << [category.name, avg, *parties.map { |e| by_party_counts[e] }]
+            csv << [category_name(category), avg, *parties.map { |e| by_party_counts[e] }]
           end
         end
+      end
+
+      private
+
+      def category_name(category)
+        category.main? ? category.name : "#{category.name} (#{category.parent.name})"
       end
 
     end
