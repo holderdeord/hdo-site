106ac38c55d7b6745832903b6c6fd0aadfc10321
Jari Bakken <jari@holderdeord.no>
2014-01-31 19:19:21 +0100

    Find all new norwegian names

--- lib/hdo/utils/proposition_source_guesser.rb
+++ lib/hdo/utils/proposition_source_guesser.rb
@@ -15,13 +15,14 @@ module Hdo
       NEGATIVES = /(?<!bokstav\s|romertall\s)/i
 
       PARTY_MAP = {
-        /\b(#{NEGATIVES}a|ap|arbeiderpartiet)\b/i        => 'A',
-        /\b(framstegspartiet|fremskrittspartiet|frp)\b/i => 'FrP',
-        /\b(høyre|høgre|#{NEGATIVES}h)\b/i             => 'H',
-        /\b(kristel(i|e)g folkeparti|krf)\b/i            => 'KrF',
-        /\b(senterpartiet|sp)\b/i                        => 'Sp',
-        /\b(sosialistisk venstreparti|sv)\b/i            => 'SV',
-        /\b(venstre|#{NEGATIVES}v)\b/i                   => 'V',
+        /\b(#{NEGATIVES}a|ap|arbeiderpartiet|arbeidarpartiet)\b/i => 'A',
+        /\b(framstegspartiet|fremskrittspartiet|frp)\b/i          => 'FrP',
+        /\b(høyre|høgre|#{NEGATIVES}h)\b/i                        => 'H',
+        /\b(kristel(i|e)g folkeparti|krf)\b/i                     => 'KrF',
+        /\b(senterpartiet|sp)\b/i                                 => 'Sp',
+        /\b(sosialistisk venstreparti|sv)\b/i                     => 'SV',
+        /\b(venstre|#{NEGATIVES}v)\b/i                            => 'V',
+        /\b(miljøpartiet dei? grønne|mdg)\b/i                     => 'MDG',
       }
 
     end
--- spec/lib/hdo/utils/proposition_source_guesser_spec.rb
+++ spec/lib/hdo/utils/proposition_source_guesser_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require './lib/hdo/utils/proposition_source_guesser'
 
 module Hdo
@@ -9,6 +10,14 @@ module Hdo
         g.parties_for('arbeiderpartiet').should == ['A']
         g.parties_for('ArbeiderpartieT').should == ['A']
         g.parties_for('Venstre').should == ['V']
+      end
+
+      it 'finds new norwegian names' do
+        g.parties_for('arbeidarpartiet').should == ['A']
+        g.parties_for('framstegspartiet').should == ['FrP']
+        g.parties_for('kristeleg folkeparti').should == ['KrF']
+        g.parties_for('høgre').should == ['H']
+        g.parties_for('miljøpartiet dei grønne').should == ['MDG']
       end
 
       it 'finds shorthand parties' do
