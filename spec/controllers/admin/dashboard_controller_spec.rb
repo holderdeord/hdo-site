28289e75a95e950ac735fac831e3e5ed3b12051b
Jari Bakken <jari@holderdeord.no>
2014-01-31 14:18:54 +0100

    Spec fix

--- spec/controllers/admin/dashboard_controller_spec.rb
+++ spec/controllers/admin/dashboard_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Admin::DashboardController do
   let(:user) { User.make! }
+  before { ParliamentSession.make!(start_date: 1.month.ago, end_date: 1.month.from_now) }
 
   it 'renders the dashboard' do
     sign_in user
