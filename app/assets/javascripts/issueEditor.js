1cc71208e750e3cf885e0d3c1baebed79dc50bef
Jari Bakken <jari@holderdeord.no>
2014-03-02 13:26:16 +0100

    JS style fixes

--- app/assets/javascripts/issueEditor.js
+++ app/assets/javascripts/issueEditor.js
@@ -1,4 +1,4 @@
-/*global HDO */
+/*global HDO, Handlebars */
 
 (function (H) {
   HDO.issueEditor = {
@@ -6,91 +6,91 @@
       var instance = Object.create(this);
       instance.root = $(selector);
       return instance;
-    }
+    },
 
-    , init: function() {
-        HDO.markdownEditor();
+    init: function () {
+      HDO.markdownEditor();
 
-        this.saveButton            = this.root.find('button[name=save]');
-        this.editorSelect          = this.root.find('select#issue_editor');
-        this.categorySelect        = this.root.find('select#issue_categories');
-        this.positionPartySelects  = this.root.find('select.position-parties');
-        this.newPositionButton     = this.root.find('#new-position');
-        this.newPartyCommentButton = this.root.find('#new-party-comment');
-        this.tagList               = this.root.find('input[name=tags]');
-        this.expandables           = this.root.find('[data-expands]');
+      this.saveButton            = this.root.find('button[name=save]');
+      this.editorSelect          = this.root.find('select#issue_editor');
+      this.categorySelect        = this.root.find('select#issue_categories');
+      this.positionPartySelects  = this.root.find('select.position-parties');
+      this.newPositionButton     = this.root.find('#new-position');
+      this.newPartyCommentButton = this.root.find('#new-party-comment');
+      this.tagList               = this.root.find('input[name=tags]');
+      this.expandables           = this.root.find('[data-expands]');
 
-        this.saveButton.click(this.save.bind(this));
-        this.newPositionButton.click(this.notImplemented.bind(this));
-        this.newPartyCommentButton.click(this.notImplemented.bind(this));
-        this.expandables.click(this.toggleRow.bind(this));
+      this.saveButton.click(this.save.bind(this));
+      this.newPositionButton.click(this.notImplemented.bind(this));
+      this.newPartyCommentButton.click(this.notImplemented.bind(this));
+      this.expandables.click(this.toggleRow.bind(this));
 
-        this.editorSelect.chosen();
-        this.categorySelect.chosen();
-        this.positionPartySelects.chosen();
+      this.editorSelect.chosen();
+      this.categorySelect.chosen();
+      this.positionPartySelects.chosen();
 
-        this.setupTagList();
-        this.setupTemplates();
-      }
+      this.setupTagList();
+      this.setupTemplates();
+    },
 
-    , save: function(e) {
-        e.preventDefault();
+    save: function (e) {
+      e.preventDefault();
+      this.toggleSpin();
+
+      // simulate xhr
+      setTimeout(function () {
         this.toggleSpin();
+      }.bind(this), 3000);
+    },
 
-        // simulate xhr
-        setTimeout(function() {
-          this.toggleSpin();
-        }.bind(this), 3000)
-      }
+    toggleSpin: function () {
+      this.root.find('.accordion-group').toggle();
+      this.root.find('.spinner').toggleClass('hidden');
+    },
 
-    , toggleSpin: function() {
-        this.root.find('.accordion-group').toggle();
-        this.root.find('.spinner').toggleClass('hidden');
-      }
+    data: function () {
+      return this.root.parent('form').serializeArray();
+    },
 
-    , data: function() {
-        return this.root.parent('form').serializeArray();
-      }
+    setupTagList: function () {
+      var el = this.tagList;
+      el.tagsManager({
+        prefilled: el.data('current-tags').split(","),
+        preventSubmitOnEnter: true,
+        typeahead: true,
+        typeaheadSource: el.data('all-tags').split(","),
+        hiddenTagListName: 'issue[tag_list]'
+      });
+    },
 
-    , setupTagList: function() {
-        var el = this.tagList;
-        el.tagsManager({
-          prefilled: el.data('current-tags').split(","),
-          preventSubmitOnEnter: true,
-          typeahead: true,
-          typeaheadSource: el.data('all-tags').split(","),
-          hiddenTagListName: 'issue[tag_list]'
-        });
-      }
+    setupTemplates: function () {
+      var templates, templateSources;
 
-    , setupTemplates: function() {
-        var templates, templateSources;
+      this.templateSources = templateSources = {
+        promiseConnection: this.root.find('#promise-connection-template').html()
+      };
 
-        this.templateSources = templateSources = {
-          promiseConnection: this.root.find('#promise-connection-template').html()
-        };
+      this.templates = templates = {};
 
-        this.templates = templates = {};
+      templates.fetch = function (name) {
+        templates[name] = templates[name] || Handlebars.compile(this.templateSources[name]);
+        return templates[name];
+      }.bind(this);
+    },
 
-        templates.fetch = function(name) {
-          templates[name] = templates[name] || Handlebars.compile(this.templateSources[name])
-          return templates[name];
-        }.bind(this);
-      }
-
-    , toggleRow: function(e) {
+    toggleRow: function (e) {
       var el = $(e.delegateTarget);
       el.find('.expandable, .expanded').toggleClass('expandable expanded');
       $(el.data('expands')).slideToggle();
-    }
+    },
 
-    , notImplemented: function(e) {
+    notImplemented: function (e) {
       e.preventDefault();
-      alert('ikke enda');
-    }
+      window.alert('ikke enda');
+    },
 
-    , newPromiseConnectionHtmlFor: function(promise) {
-        return this.templates.fetch('promiseConnection')(promise)
-      }
+    newPromiseConnectionHtmlFor: function (promise) {
+      return this.templates.fetch('promiseConnection')(promise);
+    }
   };
 }(HDO));
