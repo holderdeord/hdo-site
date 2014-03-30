8818806363a9f7665ae25070950d5254c08fe224
Jari Bakken <jari@holderdeord.no>
2014-03-30 16:11:21 +0200

    Update vagrant deploy target.

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,6 +1,6 @@
 set :git_shallow_clone, 1
 set :rails_env, :staging
 
-role :web, '192.168.1.12'
-role :app, '192.168.1.12'
-role :db,  '192.168.1.12', :primary => true
+role :web, '192.168.1.11'
+role :app, '192.168.1.11'
+role :db,  '192.168.1.11', :primary => true
