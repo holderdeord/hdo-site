b5c0be75db5c8005d11b40d72b243ff73ab064f4
Jari Bakken <jari@holderdeord.no>
2014-03-30 17:01:38 +0200

    Moving from app{1,2} => files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,17 +2,6 @@
 # app servers
 #
 
-role :app, 'app1.holderdeord.no', 'app2.holderdeord.no'
-
-#
-# all the app servers are also 'web' servers (running nginx, need maintenance page etc)
-#
-
-role :web, 'app1.holderdeord.no', 'app2.holderdeord.no'
-
-#
-# app1 doesn't really have the db, but is used to run
-# migrations and thus have the :db role in capistrano.
-#
-
-role :db, 'app1.holderdeord.no', :primary => true
+role :app, 'files.holderdeord.no'
+role :web, 'files.holderdeord.no'
+role :db, 'files.holderdeord.no', :primary => true
