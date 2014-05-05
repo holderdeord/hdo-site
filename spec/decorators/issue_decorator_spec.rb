ec8f5e37873fda1ec232be3084623cdbf3dbcf42
Jari Bakken <jari@holderdeord.no>
2014-05-05 19:42:59 +0200

    Add some specs for IssueDecorator

--- spec/decorators/issue_decorator_spec.rb
+++ spec/decorators/issue_decorator_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe IssueDecorator do
+  let(:model) { Issue.make!(promise_connections: [PromiseConnection.make!]) }
+  let(:issue) { model.decorate }
+
+  it 'has periods' do
+    issue.periods.first.should be_kind_of(IssueDecorator::Period)
+  end
+
+  it 'has knows when it was last updated' do
+    issue.updated_at.should be_kind_of(String)
+    issue.time_since_updated.should be_kind_of(String)
+  end
 end
