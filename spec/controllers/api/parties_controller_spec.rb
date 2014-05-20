f5312ca3fc9eb32951a0e4083dc08a49f84f1257
Jari Bakken <jari@holderdeord.no>
2014-05-21 00:22:54 +0200

    Add some API controller specs

--- spec/controllers/api/parties_controller_spec.rb
+++ spec/controllers/api/parties_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Api::PartiesController do
+  it 'can GET :index' do
+    get :index, format: :hal
+    response.should be_success
+  end
 
+  it 'can GET :show' do
+    pr = Party.make!
+    get :show, id: pr, format: :hal
+
+    response.should be_success
+  end
 end
--- spec/controllers/api/representatives_controller_spec.rb
+++ spec/controllers/api/representatives_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Api::RepresentativesController do
+  it 'can GET :index' do
+    get :index, format: :hal
+    response.should be_success
+  end
 
+  it 'can GET :show' do
+    pr = Representative.make!
+    get :show, id: pr, format: :hal
+
+    response.should be_success
+  end
 end
