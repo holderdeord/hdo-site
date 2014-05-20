9cf5f737fc5ac5abae643088756cfaf8c40523df
Jari Bakken <jari@holderdeord.no>
2014-05-21 00:45:35 +0200

    Travis: avoid 'The log length has exceeded the limit of 4 Megabytes'

--- script/travis/bundle_download.sh
+++ script/travis/bundle_download.sh
@@ -2,7 +2,7 @@
 
 if [[ -n "${HDO_DEPLOY_AUTH}" ]]; then
   cd $HOME
-  curl --progress-bar --user "${HDO_DEPLOY_AUTH}" "http://deploy.holderdeord.no/travis/bundle?repo_slug=${TRAVIS_REPO_SLUG}" -o bundle.tgz
+  curl --user "${HDO_DEPLOY_AUTH}" "http://deploy.holderdeord.no/travis/bundle?repo_slug=${TRAVIS_REPO_SLUG}" -o bundle.tgz
   tar xf bundle.tgz || exit 0
 else
   echo "HDO_DEPLOY_AUTH not available, not downloading bundle"
