82ee783f60538bb208ca2676dd02b8382f06433b
Jari Bakken <jari@holderdeord.no>
2015-03-03 23:14:05 +0100

    Add Link#parliament_issues

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ActiveRecord::Base
   attr_accessible :external_id, :title, :href, :link_type, :link_sub_type
   validates :href, presence: true, uniqueness: true
+
+  has_and_belongs_to_many :parliament_issues
 end
