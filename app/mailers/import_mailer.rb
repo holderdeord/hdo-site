0b8d554f3468af7f06108538a6d04441d5b7be61
Jari Bakken <jari@holderdeord.no>
2016-05-29 21:33:08 +0200

    Send import email even when there are only upcoming parliament issues.

--- app/mailers/import_mailer.rb
+++ app/mailers/import_mailer.rb
@@ -6,12 +6,28 @@ class ImportMailer < ActionMailer::Base
 
   def votes_today_email
     votes = Vote.since_yesterday
-    return unless votes.any?
+    @upcoming_issues = ParliamentIssue.since_yesterday.upcoming
+
+    if votes.empty? && @upcoming_issues.empty?
+      return
+    end
 
     @parliament_issues = votes.flat_map { |vote| vote.parliament_issues.to_a }.uniq
-    @rebel_tweets      = Hdo::Utils::RebelTweeter.since(1.day.ago).to_a
-    @upcoming_issues   = ParliamentIssue.since_yesterday.upcoming
 
-    mail subject: "#{@parliament_issues.size} nye saker behandlet"
+    counts = Hash.new(0)
+
+    if @parliament_issues.any?
+      counts['behandlet'] = @parliament_issues.size
+    end
+
+    @upcoming_issues.each do |pi|
+      counts[pi.status_name.downcase] += 1
+    end
+
+    @subject = "Nye saker fra Stortinget: " + counts.map { |key, value| "#{value} #{key}" if value > 0 }.compact.join(', ')
+
+    @rebel_tweets = Hdo::Utils::RebelTweeter.since(1.day.ago).to_a
+
+    mail subject: @subject
   end
 end
--- spec/mailers/import_mailer_spec.rb
+++ spec/mailers/import_mailer_spec.rb
@@ -1,9 +1,31 @@
 require "spec_helper"
 
+shared_examples 'mail with body' do
+  it 'creates a multipart body' do
+    expect(mail).to be_multipart
+  end
+
+  it 'has an HTML body' do
+    source = mail.parts.last.body.raw_source
+
+    expect(source).to be_kind_of(String)
+    expect(source).to_not be_empty
+  end
+
+  it 'has a text body' do
+    source = mail.parts.first.body.raw_source
+
+    expect(source).to be_kind_of(String)
+    expect(source).to_not be_empty
+  end
+end
+
 describe ImportMailer do
+  let(:mail) { ImportMailer.votes_today_email }
+
   context 'votes today' do
-    let(:mail) { ImportMailer.votes_today_email }
     let(:vote) { Vote.make!(:with_proposition) }
+    let(:parliament_issue) { ParliamentIssue.make!(:status => 'mottatt') }
 
     context 'with votes' do
       before do
@@ -14,28 +36,54 @@ describe ImportMailer do
         vote.destroy
       end
 
-      it 'creates a multipart body' do
-        mail.should be_multipart
-      end
+      it_behaves_like 'mail with body'
 
-      it 'has an HTML body' do
-        source = mail.parts.last.body.raw_source
-
-        source.should be_kind_of(String)
-        source.should_not be_empty
-      end
-
-      it 'has a text body' do
-        source = mail.parts.first.body.raw_source
-
-        source.should be_kind_of(String)
-        source.should_not be_empty
+      it 'has a subject' do
+        expect(mail.subject).to eq('Nye saker fra Stortinget: 1 behandlet')
       end
     end
 
     context 'without votes' do
       it 'is not created if there are no votes' do
-        ImportMailer.votes_today_email.to.should be_nil
+        expect(mail.to).to be_nil
+      end
+    end
+
+    context 'with new parliament issues' do
+      before do
+        parliament_issue.created_at = 23.hours.ago
+      end
+
+      after do
+        parliament_issue.destroy
+      end
+
+      it_behaves_like 'mail with body'
+
+      it 'is created with the correct subject' do
+        expect(mail.subject).to eq('Nye saker fra Stortinget: 1 mottatt')
+      end
+    end
+
+    context 'with votes and parliament issues' do
+      let(:upcoming) { ParliamentIssue.make!(:status => "til_behandling") }
+
+      before do
+        parliament_issue.created_at = 23.hours.ago
+        upcoming.created_at = 23.hours.ago
+        vote.created_at = 23.hours.ago
+      end
+
+      after do
+        parliament_issue.destroy
+        upcoming.destroy
+        vote.destroy
+      end
+
+      it_behaves_like 'mail with body'
+
+      it 'is created with the correct subject' do
+        expect(mail.subject).to eq('Nye saker fra Stortinget: 1 behandlet, 1 mottatt, 1 til behandling')
       end
     end
 
