585acb4afe2200016cf41e3939dc620b2f6d8460
Jari Bakken <jari@holderdeord.no>
2017-10-15 23:06:39 +0200

    Fix source guesser for Rødt

--- spec/lib/hdo/utils/proposition_source_guesser_spec.rb
+++ spec/lib/hdo/utils/proposition_source_guesser_spec.rb
@@ -10,6 +10,7 @@ module Hdo
         g.parties_for('arbeiderpartiet').should == ['A']
         g.parties_for('ArbeiderpartieT').should == ['A']
         g.parties_for('Venstre').should == ['V']
+        g.parties_for('Rødt').should == ['R']
       end
 
       it 'finds new norwegian names' do
@@ -17,7 +18,7 @@ module Hdo
         g.parties_for('framstegspartiet').should == ['FrP']
         g.parties_for('kristeleg folkeparti').should == ['KrF']
         g.parties_for('høgre').should == ['H']
-        g.parties_for('miljøpartiet dei grønne').should == ['MDG']
+        g.parties_for('raudt').should == ['R']
       end
 
       it 'finds shorthand parties' do
@@ -25,10 +26,12 @@ module Hdo
         g.parties_for('ap').should == ['A']
         g.parties_for('krf').should == ['KrF']
         g.parties_for('frp').should == ['FrP']
+        g.parties_for('r').should == ['R']
       end
 
       it 'looks at boundaries' do
         g.parties_for('sosialistisk venstreparti').should_not include("V")
+        g.parties_for('rød valgallianse').should_not include("R")
       end
 
       it 'ignores "bokstav" and "romertall"' do
