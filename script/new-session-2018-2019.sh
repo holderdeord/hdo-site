6b87179341e6c14f951bb5806c102616a8812260
Jari Bakken <jari@holderdeord.no>
2018-10-01 23:49:08 +0200

    Fix new session script

--- script/new-session-2018-2019.sh
+++ script/new-session-2018-2019.sh
@@ -3,10 +3,10 @@
 set -e
 set -x
 
-# bundle exec script/import parliament-sessions && \
-#   bundle exec script/import parliament-periods && \
-#   bundle exec script/import parties && \
-#   bundle exec script/import daily --period 2017-2021 --session 2017-2018 && \
+bundle exec script/import parliament-sessions && \
+  bundle exec script/import parliament-periods && \
+  bundle exec script/import parties && \
+  bundle exec script/import daily --period 2017-2021 --session 2017-2018 && \
   bundle exec script/import daily --period 2017-2021 --session 2018-2019 && \
   bundle exec rake images:party_logos && \
   bundle exec rake images:all && \
