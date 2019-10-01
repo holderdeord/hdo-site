f2dbd1452f069ae2b16865211c0115c7ddad9f00
Jari Bakken <jari@holderdeord.no>
2019-10-01 11:54:47 +0200

    update new session script

--- script/new-session.sh
+++ script/new-session.sh
@@ -6,8 +6,8 @@ set -x
 bundle exec script/import parliament-sessions && \
   bundle exec script/import parliament-periods && \
   bundle exec script/import parties && \
-  bundle exec script/import daily --period 2017-2021 --session 2017-2018 && \
   bundle exec script/import daily --period 2017-2021 --session 2018-2019 && \
+  bundle exec script/import daily --period 2017-2021 --session 2019-2020 && \
   bundle exec rake images:party_logos && \
   bundle exec rake images:all && \
   bundle exec rake search:reindex && \
